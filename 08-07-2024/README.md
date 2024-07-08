e15840b7b08ff4f61b22bd192ee2ff009b5811bf
Ania <beninatistefania@gmail.com>
2024-07-08 17:36:59 +0200

    feat: add commit 08-07-2024

--- 08-07-2024/README.md
+++ 08-07-2024/README.md
@@ -1,8 +1,46 @@
-# React + Vite
+# Frontend Mentor - Social links profile
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Realizzare una todolist come quella che vi lascio in in allegato nell'immagine; nell'input dovete scrivere il titolo della singola todo che al click del bottone dovrà essere aggiunta alla lista di todos.
 
-Currently, two official plugins are available:
+## Panoramica
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### La sfida
+
+Esercizio facoltativo uno:
+aggiungere un bottone alle todos che al click, cancella la specifica todo dalla lista.
+
+Esercizio facoltativo due:
+inserire input di tipo testuale e di tipo submit all'interno di una form e fare in modo che funzioni tutto perfettamente. 
+
+### Screenshot dell'esempio da seguire
+
+![Url immagine challenge](./src/assets/image.png)
+
+#Struttura Progetto
+
+my-todo-app/
+├── src/
+│   ├── index.css
+│   ├── app.module.css
+│   ├── app.jsx
+│   ├── main.jsx
+├── package.json
+├── package-lock.json
+├── .gitignore
+└── README.md
+
+
+## Il mio processo
+
+### Struttura del Progetto
+
+HTML:
+Costruisci la struttura HTML seguendo il layout del design. Assicurati di includere tutti gli elementi richiesti come le icone dei social media, i testi, ecc.
+
+CSS:
+Utilizza i dettagli della guida di stile per applicare i colori, i font e altri stili. Definisci gli stati di hover e focus per gli elementi interattivi.
+
+
+Autore
+
+Stefania Beninati 
--- 08-07-2024/src/App.jsx
+++ 08-07-2024/src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import styles from "./app.module.css";
+import { useState } from "react";
 
+//definisco un array iniziale di todos
+const initialTodos = [
+  {
+    id: self.crypto.randomUUID(),
+    title: "Fare la spesa",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    title: "Pagare bolletta",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    title: "Andare dal meccanico",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    title: "Farmacia",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    title: "Chiama Pippo",
+  },
+];
+
+//utilizzo lo stato per gestire la lista dei todos e l'input dell'utente
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState(initialTodos);
+  const [input, setInput] = useState("");
+  
+  /**
+  * Aggiunge un nuovo todo alla lista
+  * @param {Event} e - L'evento di submit del form
+  */
+  const addTodo = (e) => {
+    e.preventDefault(); //previene il comportamento predefinito del form (che farebbe ricaricare la pagina)
+    if (!input.length) return; // Verifica se l'input non è vuoto
+    
+    const id = self.crypto.randomUUID(); // Genero un ID univoco e creo un nuovo todo con il titolo dell'input
+    const title = input;
+    setTodos([...todos, { id, title }]);
+    setInput("");
+  };
+  
+  /**
+  * Gestisce il cambiamento del valore dell'input
+  * @param {Event} e - L'evento di cambio dell'input
+  */
+  const handleChange = (e) => {
+    setInput(e.target.value); // Aggiorna lo stato dell'input con il nuovo valore
+  };
+  
+  /**
+  * Cancella uno specifico todo dalla lista
+  * @param {string} id - L'ID del todo da cancellare
+  */
+  const handleDelete = (id) => {
+    const newList = todos.filter((todo) => todo.id !== id);
+    setTodos(newList);
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={styles.container}>
+    <form onSubmit={addTodo}>
+    {/* Input per inserire il titolo del nuovo todo e dove inserisco un input di tipo testuale e di tipo submit all'interno del form*/}
+    <input
+    value={input}
+    placeholder="Input"
+    onChange={handleChange}
+    className={styles.input}
+    type={"text"}
+    />
+    {/* Bottone per aggiungere il nuovo todo */}
+    <button type="submit" className={styles.addButton}>
+    ADD
+    </button>
+    </form>
+    <div className={styles.container__list}>
+    <ul>
+    {/* Mappa i todos e crea un elemento della lista per ciascuno */}
+    {todos.map((todo) => (
+      <li key={todo.id} className={styles.todoItem}>
+      {todo.title}
+      {/* Ho aggiunto qui il Bottone per cancellare lo specifico todo dalla lista*/}
+      <button
+      className={styles.deleteButton}
+      onClick={() => handleDelete(todo.id)}
+      >
+      Delete
+      </button>
+      </li>
+    ))}
+    </ul>
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
