15528245797b780fd884bfa0b2d20ade55f4348b
Ania <beninatistefania@gmail.com>
2024-07-23 16:59:03 +0200

    feat: add detail + error + loading handling

--- 22-07-2024/src/api/perfumeClient.js
+++ 22-07-2024/src/api/perfumeClient.js
@@ -31,6 +31,25 @@ export const getPerfumeList = () => {
     });
 };
 
+export const getPerfumeDetail = (id) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                id: "1",
+                title: "Esempio Profumo 1",
+                producer: "Casa Profumi 1",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                price: 50.0,
+                fragrances: ["Rosa", "Lavanda"],
+                for_men: true,
+                for_women: false,
+                for_kids: false,
+                image_url: "https://images.unsplash.com/photo-1701291927826-c7775869d822?q=80&w=1910&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            });
+        }, 500);
+    });
+};
+
 /* 
 export const getPerfumeList = async () => {
     try {
@@ -42,11 +61,4 @@ export const getPerfumeList = async () => {
 }; 
 */
 
-export const labels = {
-    perfumeTableTitle: "Title",
-    perfumeTableProducer: "Producer",
-    perfumeTableDescription: "Description",
-    perfumeTablePrice: "Price",
 
-    perfumeList: "Perfume List"
-};
--- 22-07-2024/src/main.jsx
+++ 22-07-2024/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import DefaultLayout from "./layouts/DefaultLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PerfumeDetailPage from "./pages/PerfumeDetailPage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+            },
+            {
+                path: "perfumes/:id",
+                element: <PerfumeDetailPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
