aff6237d5b06a0c7804bd32e49e513af20947ed5
Ania <beninatistefania@gmail.com>
2024-08-30 19:53:55 +0200

    feat: add cover

--- 30-08-2024/app/page.tsx
+++ 30-08-2024/app/page.tsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div className="flex justify-center items-center h-full p-8">
-      <Image 
-        src="https://lh4.googleusercontent.com/proxy/ifcDFsRHpXzKeHktR8wErYSkLCn0H2LMZMSMRb9lR6VaNjYY8OKwLGXt7XeVYjVakIBBwGHQnmFisOho0vlH49vL_hpogPkCtBRarBKwdM0XzwIzp4uALU0OaPr44BVVqQM2m7bc9OIr=w1200-h630-p-k-no-nu" 
-        alt="Home Image"
-        width={1400} 
-        height={1400} 
-        className="max-w-full max-h-full object-contain"
-        priority 
-      />
+    <div className="flex-grow flex justify-center items-center overflow-hidden">
+      <div className="relative w-full h-[calc(100vh-64px-32px)]">
+        <Image 
+          src="https://lh4.googleusercontent.com/proxy/ifcDFsRHpXzKeHktR8wErYSkLCn0H2LMZMSMRb9lR6VaNjYY8OKwLGXt7XeVYjVakIBBwGHQnmFisOho0vlH49vL_hpogPkCtBRarBKwdM0XzwIzp4uALU0OaPr44BVVqQM2m7bc9OIr=w1200-h630-p-k-no-nu" 
+          alt="Home Image"
+          fill
+          style={{ 
+            objectFit: 'cover',
+            objectPosition: 'center 90%' // Questo sposta l'immagine leggermente verso l'alto
+          }}
+          priority 
+        />
+      </div>
     </div>
   );
 };
